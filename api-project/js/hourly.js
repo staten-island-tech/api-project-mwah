ee79dc9f8d8b88c9f6b4df60f4321a8141de9ddb
uh-karsi <90662606+uh-karsi@users.noreply.github.com>
2021-12-15 13:13:37 -0500

    music start

---
+++ api-project/js/hourly.js
@@ -0,0 +1,21 @@
+const hours = {
+    zero:
+}
+
+function one(min, max) {
+  min = Math.ceil(4);
+  max = Math.floor(6);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function one(min, max) {
+  min = Math.ceil(4);
+  max = Math.floor(6);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function one(min, max) {
+  min = Math.ceil(4);
+  max = Math.floor(6);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
---
+++ api-project/js/main.js
@@ -0,0 +1,54 @@
+import "../styles/style.css";
+import "";
+
+const villagers = "http://acnhapi.com/v1/villagers/";
+const songs = "http://acnhapi.com/v1/songs/";
+const bgm = "http://acnhapi.com/v1/backgroundmusic/";
+
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData(villagers);
+getData(songs);
+getData(bgm);
+
+var minutesLabel = document.getElementById("minutes");
+var secondsLabel = document.getElementById("seconds");
+var hoursLabel = document.getElementById("hours");
+var totalSeconds = 3800;
+setInterval(setTime, 1000);
+
+function setTime() {
+  ++totalSeconds;
+  secondsLabel.innerHTML = pad(totalSeconds % 60);
+  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+  hoursLabel.innerHTML = pad(parseInt(totalSeconds / 3600));
+}
+setTime();
+function pad(val) {
+  var valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+}
+
+function updateDaMusic(hour) {
+  document.body.insertAdjacentHTML(
+    "afterbegin",
+    `<audio
+  controls autoplay loop
+  src="https://acnhapi.com/v1/hourly/${hour}">
+      Your browser does not support the
+      <code>audio</code> element.
+</audio>`
+  );
+}
+updateDaMusic(one());
