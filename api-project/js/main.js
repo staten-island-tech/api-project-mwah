d9c6ddee1f7a2f12daad094e5f26ae7273f91f6a
uh-karsi <90662606+uh-karsi@users.noreply.github.com>
2021-12-17 12:39:12 -0500

    im really trying

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,5 +1,6 @@
 import "../styles/style.css";
 import { hours } from "./hourly";
+import { update } from "./update";
 
 const villagers = "http://acnhapi.com/v1/villagers/";
 const songs = "http://acnhapi.com/v1/songs/";
@@ -21,7 +22,7 @@ getData(bgm);
 var minutesLabel = document.getElementById("minutes");
 var secondsLabel = document.getElementById("seconds");
 var hoursLabel = document.getElementById("hours");
-var totalSeconds = 3600;
+var totalSeconds = 0;
 setInterval(setTime, 1000);
 
 function setTime() {
@@ -51,84 +52,6 @@ function insertDaMusic(hour) {
       <code>audio</code> element.
 </audio>`
   );
-}
-
-function updateDaMusic() {
-  if (hoursLabel.innerHTML === "00") {
-    insertDaMusic(hours.zero());
-    console.log("zero");
-  } else if (hoursLabel.innerHTML === "01") {
-    insertDaMusic(hours.one());
-    console.log("one");
-  } else if (hoursLabel.innerHTML === "02") {
-    insertDaMusic(hours.two());
-    console.log("two");
-  } else if (hoursLabel.innerHTML === "03") {
-    insertDaMusic(hours.three());
-    console.log("three");
-  } else if (hoursLabel.innerHTML === "04") {
-    insertDaMusic(hours.four());
-    console.log("four");
-  } else if (hoursLabel.innerHTML === "05") {
-    insertDaMusic(hours.five());
-    console.log("five");
-  } else if (hoursLabel.innerHTML === "06") {
-    insertDaMusic(hours.six());
-    console.log("six");
-  } else if (hoursLabel.innerHTML === "07") {
-    insertDaMusic(hours.seven());
-    console.log("seven");
-  } else if (hoursLabel.innerHTML === "08") {
-    insertDaMusic(hours.eight());
-    console.log("eight");
-  } else if (hoursLabel.innerHTML === "09") {
-    insertDaMusic(hours.nine());
-    console.log("nine");
-  } else if (hoursLabel.innerHTML === "10") {
-    insertDaMusic(hours.ten());
-    console.log("ten");
-  } else if (hoursLabel.innerHTML === "11") {
-    insertDaMusic(hours.eleven());
-    console.log("eleven");
-  } else if (hoursLabel.innerHTML === "12") {
-    insertDaMusic(hours.twelve());
-    console.log("twelve");
-  } else if (hoursLabel.innerHTML === "13") {
-    insertDaMusic(hours.thirteen());
-    console.log("thirteen");
-  } else if (hoursLabel.innerHTML === "14") {
-    insertDaMusic(hours.fourteen());
-    console.log("fourteen");
-  } else if (hoursLabel.innerHTML === "15") {
-    insertDaMusic(hours.fifteen());
-    console.log("fifteen");
-  } else if (hoursLabel.innerHTML === "16") {
-    insertDaMusic(hours.sixteen());
-    console.log("sixteen");
-  } else if (hoursLabel.innerHTML === "17") {
-    insertDaMusic(hours.seventeen());
-    console.log("seventeen");
-  } else if (hoursLabel.innerHTML === "18") {
-    insertDaMusic(hours.eighteen());
-    console.log("eighteen");
-  } else if (hoursLabel.innerHTML === "19") {
-    insertDaMusic(hours.nineteen());
-    console.log("nineteen");
-  } else if (hoursLabel.innerHTML === "20") {
-    insertDaMusic(hours.twenty());
-    console.log("twenty");
-  } else if (hoursLabel.innerHTML === "21") {
-    insertDaMusic(hours.twentyone());
-    console.log("twentyone");
-  } else if (hoursLabel.innerHTML === "22") {
-    insertDaMusic(hours.twentytwo());
-    console.log("twentytwo");
-  } else if (hoursLabel.innerHTML === "23") {
-    insertDaMusic(hours.twentythree());
-    console.log("twentythree");
-  } else {
-    console.log("error");
-  }
 }
 
 updateDaMusic();
